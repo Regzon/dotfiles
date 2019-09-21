4624e55adb00e94bbe8691ff655d597ba607da67
Andrey Volkov <andrew.wolf.alex@gmail.com>
2019-09-22 02:04:34 +0300

    Fix installation script

--- install.sh
+++ install.sh
@@ -6,12 +6,21 @@ ln -sf $SCRIPTPATH/.bashrc $HOME/.bashrc
 
 ln -sf $SCRIPTPATH/.tmux.conf $HOME/.tmux.conf
 
+if [ -e "$HOME/.themes" ]; then
+    rm -r $HOME/.themes
+fi
 ln -sf $SCRIPTPATH/.themes $HOME/.themes
 
+if [ -e "$HOME/.icons" ]; then
+    rm -r $HOME/.icons
+fi
 ln -sf $SCRIPTPATH/.icons $HOME/.icons
 
 mkdir -p $HOME/.vim
 ln -sf $SCRIPTPATH/.vim/vimrc $HOME/.vim/vimrc
 
-mkdir -p $HOME/.config/dconf
-ln -sf $SCRIPTPATH/.config/dconf/user $HOME/.config/dconf/user
+mkdir -p $HOME/.config
+if [ -e "$HOME/.config/dconf" ]; then
+    rm -r $HOME/.config/dconf
+fi
+ln -sf $SCRIPTPATH/.config/dconf $HOME/.config/dconf
